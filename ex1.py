5a871ca8bab26ede7ac2951208f9b4e96518728f
Auto Logon <autologon@CSED.LAB>
2023-08-28 17:30:07 -0300

    Exercicios wokshop

---
+++ ex1.py
@@ -0,0 +1,33 @@
+# q1
+'''num1 = int(input("Digite um numero: "))
+print(f"O sucessor é: {num1+1}")
+print(f"O antecessor é: {num1-1}")'''
+
+#Q2
+'''num1 = float (input ("Digite um numero: "))
+num2 = float (input("Digite outro numero: "))
+
+soma = num1 + num2 
+print(f"Soma dos numeros {num1} e {num2} é igual a: {soma}")
+substracao = num1 - num2
+print(f"Substraçao dos numeros {num1} e {num2} é igual a: {substracao}")
+multiplicacao = num1 * num2
+print(f"Multiplicaçao dos numeros {num1} e {num2} é igual a: {multiplicacao}")
+divicao = num1 / num2
+print(f"Diviçao dos numeros {num1} e {num2} é igual a: {divicao}")
+'''
+#Q3
+"""num1 = float(input("Adicione um valor ao primer numero: "))
+num2 = float(input("Adicione um valor pro outro número: "))
+
+if num1 > num2 :
+    print("True")
+
+else :
+    print("False")"""
+
+#4
+grausc = float (input("Digite uma temperatura em graus celsius: "))
+
+grausf = (grausc * 9/5) + 32
+print(f"A temperatura em graus fahrenheit é: {grausf}")
