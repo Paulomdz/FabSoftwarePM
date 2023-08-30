977718dfdf3a427dbb962182b08299de676bf0f5
Paulomdz <134633938+Paulomdz@users.noreply.github.com>
2023-08-30 16:21:18 -0300

    Add files via upload

---
+++ 4listas.py
@@ -0,0 +1,20 @@
+'''lista = []
+controle = None
+controle = True
+acao = None
+indice = 0
+while controle:
+  acao = int(input('Digite o que você quer fazer:'))
+  
+  if acao == 3:
+      controle = False
+      print('O programa foi encerrado!')
+  else:
+    if acao == 1:
+      print('digite que tarefa você quer adicionar:')
+      tarefa = input()
+      lista.append(tarefa)
+    elif acao == 2:
+        print('Executando...')
+        print(lista[indice])
+        indice += 1'''
